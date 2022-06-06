4d978e8ddd54fe2dc2f6066ac4157751b2054b50
Ahmed Hany <ahmed.hany@integrant.com>
2022-06-06 17:43:58 +0200

    updates

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from setuptools import setup, find_packages
+import sys
+
+from setuptools import setup
+from piplapis import __version__
+
+
+py_version = sys.version_info[:2]
+if py_version[0] < 3 and py_version not in [(2, 6), (2, 7)]:
+    raise RuntimeError("Python 2.6 and up is required")
+
 
 setup(
-    name = 'Moriarty',
-    version = '1.0.0',
-    url = '',
-    description = '',
-    packages = find_packages(),
-    install_requires = [
-        # Github Private Repository
-    ]
+    name="moriarty",
+    version=__version__,
+    author="",
+    author_email="",
+    description="Client library for finding important information about a spesific phone number.",
+    url="",
+    license="",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[],
+    packages=["moriarty_project"],
 )
