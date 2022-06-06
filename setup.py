48e1d55b01aff4d3a38032145c0482bb7464e5b3
Ahmed Hany <ahmed.hany@integrant.com>
2022-06-06 17:30:30 +0200

    replace setupt file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Moriarty',
+    version = '1.0.0',
+    url = '',
+    description = '',
+    packages = find_packages(),
+    install_requires = [
+        # Github Private Repository
+    ]
+)
